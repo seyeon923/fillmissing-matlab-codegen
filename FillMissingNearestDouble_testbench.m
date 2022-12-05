9fd0564e281c4481a0d4bd1f83925a3b7380ed59
Kim Se-yeon <tpdussla93@gmail.com>
2022-12-05 16:32:44 +0900

    Add codegen project

--- FillMissingNearestDouble_testbench.m
+++ FillMissingNearestDouble_testbench.m
@@ -1,7 +1,7 @@
 function FillMissingNearestDouble_TestBench()
     x = [1.,2.,nan; 4.,5.,6.]
-    y = FillMissingNearestDouble(x, 'nearest')
+    y = FillMissingNearestDouble(x)
 
     x = [1.,2.,3.;4.,nan,5.;6.,7.,8.]
-    y = FillMissingNearestDouble(x, 'nearest')
+    y = FillMissingNearestDouble(x)
 end
